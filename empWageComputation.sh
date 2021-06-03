5cc838ce5f363f25875505089abdd191469dee54
Minal <patilminalp@gmail.com>
2021-06-04 02:44:07 +0530

    Added:

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,45 +1,33 @@
-x=$((RANDOM%2))
-case "$x" in
-"1")
-echo employee is present
-;;
-"0")
-echo employee is absent
-;;
-esac
+
 
 FullDayHr=8
 PartTimeHr=4
-wageperHr=20
-month=1
+WagePerHr=20
+month=0
 totalhr=0
 totalwage=0
 
+function WorkHrs
+{
 while [[ $month != 21 ]];do
 
 a=$((4+((4*$((RANDOM%2))))))
 case "$a" in
 "8")
-echo wage for fullday: "$(($FullDayHr*$wageperHr))"
 totalhr=$(($totalhr+$a))
 ((month++))
 ;;
 
 "4")
-echo part time wage  : "$(($PartTimeHr*$wageperHr))"
 totalhr=$(($totalhr+$a))
 ((month++))
 ;;
-
 esac
-
-if [ $totalhr -ge 100 ]
-then
-     break
-fi
 done
-totalwage=$(($totalhr*20))
-echo wage for working for a month:"$totalwage"
+echo total work hours are "$totalhr"
+}
+
+WorkHrs
 
 
 
