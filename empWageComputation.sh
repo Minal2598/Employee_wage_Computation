81e63e5984ba7a9ef1ef03e6c9cc8c35386af0fb
Minal <patilminalp@gmail.com>
2021-06-03 21:06:04 +0530

    Added:using switch Case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,17 +3,22 @@ isFullTime=2;
 WagePerHr=20;
 empHr=0;
 TotalWage=0;
-random=$(($RANDOM%3));
+empCheck=$(($RANDOM%3));
 
-if [ $isFullTime -eq $random ];
-then
-       empHr=8;
-elif [ $isPartTime -eq $random ];
-then
-       empHr=4;
-else
-     empHr=0;
-fi
+case $empCheck in
+	 $isFullTime)
+		empHr=8
+		;;
+	$isPartTime)
+		empHr=4
+		;;
+	*)
+	  empHr=0
+	        ;;
+esac
+
+
 TotalWage=$(($WagePerHr*$empHr))
 echo "DailyWage of Employee::"$TotalWage
+
 
