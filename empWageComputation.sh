b28f55e35769588e45a4533d46f173f5c208f28f
Minal <patilminalp@gmail.com>
2021-06-03 18:25:03 +0530

    Added:Emp Present or Absent

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,11 @@
 echo "Welcome to Employee Wage Computation"
+
+isPresent=1
+random=$(($RANDOM%2))
+
+if [ $isPresent -eq $random ]
+then
+     echo "Employee is Present"
+else
+     echo "Employee is Absent"
+fi
