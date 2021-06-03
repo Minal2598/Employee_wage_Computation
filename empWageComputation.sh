c58a153f520e41f2cb2c94b39e3a2283849b9ee3
Minal <patilminalp@gmail.com>
2021-06-03 21:33:58 +0530

    Added:Cal Wages for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,24 +1,26 @@
 isPartTime=1;
 isFullTime=2;
 WagePerHr=20;
+numWorkingDay=20
 empHr=0;
 TotalWage=0;
-empCheck=$(($RANDOM%3));
 
-case $empCheck in
-	 $isFullTime)
-		empHr=8
-		;;
-	$isPartTime)
-		empHr=4
-		;;
-	*)
-	  empHr=0
-	        ;;
-esac
+for((day=1;day<=$numWorkingDay;day++))
+do
+   empCheck=$(($RANDOM%3));
 
-
-TotalWage=$(($WagePerHr*$empHr))
-echo "DailyWage of Employee::"$TotalWage
-
+	case $empCheck in
+		 $isFullTime)
+			empHr=8
+			;;
+		$isPartTime)
+			empHr=4
+			;;
+		*)
+	  	empHr=0
+	        	;;
+	esac
+	TotalWage=$(($WagePerHr*$empHr))
+	echo "DailyWage of Employee::"$TotalWage
+done
 
