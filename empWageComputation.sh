114fdd0c274d83adf6a55a8e3d8ab69293061392
Minal <patilminalp@gmail.com>
2021-06-04 16:40:24 +0530

    Added:the Day and

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,5 +1,5 @@
-declare -A dandtwage
-
+declare -A dailyandtotalwage
+declare -A Day
 FulldayHr=8
 PartTimeHr=4
 WagePerHr=20
@@ -8,37 +8,65 @@ totalhr=0
 totalwage=0
 day=0
 dailywage=0
+check=0
 
 function WorkHrs
 {
 while [[ $month != 21 ]];do
 
-a=$((4+((4*$((RANDOM%2))))))
-case "$a" in
-"8")
-dailywage=$(($a*20))
-totalhr=$(($totalhr+$a))
-	dandtwage[$day]=$dailywage
-echo dailywage :${dandtwage[$day]}
-((day++))
-((month++))
-;;
+   a=$((4+((4*$((RANDOM%2))))))
 
-"4")
-dailywage=$(($a*20))
-totalhr=$(($totalhr+$a))
-dandtwage[$day]=$dailywage
-echo dailywage :${dandtwage[$day]}
-((day++))
-((month++))
-;;
-esac
-done
+   check=$(($month%5))
 
-echo total work hours are "$totalhr"
-totalwage=$(($totalhr*20))
-dandtwage[$day]=$totalwage
-echo total wage : ${dandtwage[$day]}
+   case "$check" in
+      "1")
+		Day[$month]="Monday"
+ 		echo "Monday"
+       ;;
+      "2")
+		Day[$month]="Tuesday"
+        	echo "Tuesday"
+       ;;
+      "3")
+		Day[$month]="Wednusday"
+		echo "Wednusday"
+       ;;
+      "4")
+		Day[$month]="Thursday"
+		echo "Thursday"
+       ;;
+      "0")
+		Day[$month]="Friday"
+		echo "Friday"
+       ;;
+   esac
+
+
+    case "$a" in
+       "8")
+    		dailywage=$(($a*20))
+    		totalhr=$(($totalhr+$a))
+    		dailyandtotalwage[$day]=$dailywage
+		echo dailywage :${dailyandtotalwage[$day]}
+		((day++))
+		((month++))
+	;;
+
+	"4")
+		dailywage=$(($a*20))
+		totalhr=$(($totalhr+$a))
+		dandtwage[$d]=$dailywage
+		echo dailywage :${dailyandtotalwage[$day]}
+		((day++))
+		((month++))
+ 	;;
+    esac
+    done
+
+ echo total work Hrs are "$totalhr"
+ totalwage=$(($totalhr*20))
+ dailyandtotalwage[$day]=$totalwage
+ echo total wage : ${dailyandtotalwage[$day]}
 }
 
 WorkHrs
